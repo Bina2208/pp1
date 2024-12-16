7054ff6cc10c561357ad36b26d36c8e5f471665a
Bina2208 <83944511+Bina2208@users.noreply.github.com>
2024-12-16 16:06:26 +0100

    Add example for additonal Bar charts

--- plotly/example-barChart.py
+++ plotly/example-barChart.py
@@ -1,6 +1,26 @@
+import pandas as pd
 import plotly.express as px
 
-fig = px.bar(x=["a", "b", "c"], y=[1, 3, 2]);
-fig.write_html("first_figure.html", auto_open=True)
+# Datei laden (ersetze 'datei.csv' mit dem Pfad zu deiner Datei)
+datei_pfad = '../Dataset1-EVs_and_chargers_by_state.csv'
+df = pd.read_csv(datei_pfad, thousands=',', decimal='.')
+
+# Prüfen ob die Datei geladen wurde
+print(df.head())
+# Datentypen anzeigen
+print("Types: ", df.dtypes)
+
+# Entfernen der Zeilen für Median, Average und Total
+filter_values = ['Median', 'Average', 'Total']
+df_filtered = df[~df['State'].isin(filter_values)].copy()
+
+
+
+fig = px.bar(
+    df_filtered,
+    title="EVs and chargers by state",
+    x="State", 
+    y=["Total Evs", "Total Chargers"],
+    labels={'value': 'Number of Chargers'})
 
 fig.show()
